edd8cbf4aa0de67d67a80d130574c88d76c473cc
Agustin Belizan <agustinbelizan@icloud.com>
2022-08-20 21:10:35 -0300

    Desafío: Crea tu landing

--- eccom-nozomi/src/components/CartWidget.js
+++ eccom-nozomi/src/components/CartWidget.js
@@ -1,7 +1,8 @@
 const CartWidget = () => {
     return (
-        <div>
-            <i className="bi bi-bag-check-fill"></i>
+        <div className="col-2 text-center">
+            <span><i className="bi bi-bag-check-fill fs-1"></i></span>
+
         </div>
     )
 }
--- eccom-nozomi/src/components/NavBar.js
+++ eccom-nozomi/src/components/NavBar.js
@@ -2,14 +2,17 @@ import CartWidget from "./CartWidget"
 
 const NavBar = () => {
     return (
-        <div>
-            <nav className="navbar navbar-dark bg-primary">
+        <div className="grid">
+            <nav className="navbar navbar-dark bg-primary grid">
                 <CartWidget />
-                <ul className="text-light">
-                    <li><a className="text-light" href="./index.html">home</a></li>
-                    <li><a className="text-light" href="./html/carta.html">carta</a></li>
-                    <li><a className="text-light" href="./html/ofertas.html">ofertas</a></li>
-                    <li><a className="text-light" href="./html/nosotros.html">nosotros</a></li>
+                <ul className="text-light text-center m-0 col-10 list-inline" data-bs-toggle="tooltip">
+                    <div className="row">
+                        <li className="col"><a className="nav-link text-light" href="./index.html">home</a></li>
+                        <li className="col"><a className="nav-link text-light" href="./html/carta.html">carta</a></li>
+                        <li className="col"><a className="nav-link text-light" href="./html/ofertas.html">ofertas</a></li>
+                        <li className="col"><a className="nav-link text-light" href="./html/nosotros.html">nosotros</a></li>
+                    </div>
+
                 </ul>
             </nav>
         </div>
