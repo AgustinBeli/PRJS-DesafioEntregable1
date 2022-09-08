701dd63c6414ab71bfd08feafd47ef3f662a28a0
Agustin Belizan <agustinbelizan@icloud.com>
2022-09-08 02:32:31 -0300

    Primera Entrega del Proyecto Final

--- eccom-nozomi/src/components/CartWidget.js
+++ eccom-nozomi/src/components/CartWidget.js
@@ -1,6 +1,6 @@
 const CartWidget = () => {
     return (
-        <div className="col-2 text-center">
+        <div>
             <span><i className="bi bi-bag-check-fill fs-1"></i></span>
         </div>
     )
--- eccom-nozomi/src/components/NavBar.js
+++ eccom-nozomi/src/components/NavBar.js
@@ -4,13 +4,14 @@ import CartWidget from "./CartWidget"
 
 const NavBar = () => {
     return (
-        <div className="grid">
+        <div>
             <nav className="navbar navbar-dark bg-primary grid">
-                <Link className="col-6" to='/' style={{ textDecoration: "none", color: "white" }}><CartWidget /></Link>
-                <ul className="text-light text-center m-0 col-6 list-inline" data-bs-toggle="tooltip">
-                    <div className="row">
-                        <Link className="col" to='/category/1' style={{ textDecoration: "none", color: "white" }}><h2 className="nav-link text-light">APPLE</h2></Link>
-                        <Link className="col" to='/category/2' style={{ textDecoration: "none", color: "white" }}><h2 className="nav-link text-light">XIAOMI</h2></Link>
+                <ul className="text-light text-center m-0 col-12 list-inline" data-bs-toggle="tooltip">
+                    <div className="row d-flex align-items-center">
+                        <h1 className="col-2">NOZOMI</h1>
+                        <Link className="col-4" to='/category/1' style={{ textDecoration: "none", color: "white" }}><h2 className="nav-link text-light">APPLE</h2></Link>
+                        <Link className="col-4" to='/category/2' style={{ textDecoration: "none", color: "white" }}><h2 className="nav-link text-light">XIAOMI</h2></Link>
+                        <Link className="col-2" to='/' style={{ textDecoration: "none", color: "white" }}><CartWidget /></Link>
                     </div>
                 </ul>
             </nav >
